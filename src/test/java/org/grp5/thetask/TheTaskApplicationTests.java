a6a6a939d0a3550fce11affa7eb472319b2c2b75
Faraz Rostami <faraz.rostami@gmail.com>
2024-11-26 18:08:07 +0100

    adds testAddTaskIncreasesSize-test in TheTaskApplicationTests.java

--- src/test/java/org/grp5/thetask/TheTaskApplicationTests.java
+++ src/test/java/org/grp5/thetask/TheTaskApplicationTests.java
@@ -1,5 +1,10 @@
 package org.grp5.thetask;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +13,20 @@ class TheTaskApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testAddTaskIncreasesSize() {
+        // Adds an empty list for new tasks
+        List<Object> tasks = new ArrayList<>(); 
+    
+        // Adds a new task to the list
+        int initialSize = tasks.size(); // Gets the initial size of tasks list
+        tasks.add(new Object()); // A new task is added to the list
+        int newSize = tasks.size(); // Get the new size of tasks list
+    
+        // Controls that the initial size has been incremented by 1
+        assertEquals(initialSize + 1, newSize);
     }
 
 }
