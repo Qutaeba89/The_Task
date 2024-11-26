3987b235c7d1696d6ffa567fccd636c8cb007fde
Kaarin1337 <kaarin.gaming@tutanota.com>
2024-11-26 10:41:36 +0100

    first push

---
+++ src/main/java/org/grp5/thetask/TheTaskApplication.java
@@ -0,0 +1,13 @@
+package org.grp5.thetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/grp5/thetask/TheTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package org.grp5.thetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
