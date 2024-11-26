b6e71d14e4988a44c4616e29698f966c2e1ffa05
Kaarin1337 <kaarin.gaming@tutanota.com>
2024-11-26 14:51:13 +0100

    List Class added

---
+++ src/main/java/org/grp5/thetask/TodoList.java
@@ -0,0 +1,17 @@
+package org.grp5.thetask;
+
+
+public class TodoList {
+//    private ArrayList<TodoTask> tasks;
+
+    //Assume we get time in milliSec.
+    public void createTodoTask(String title, long timeInMilliSecDeadLine){
+
+    }
+
+//    public ArrayList<ToDoTask> getTodoTasks(){
+//
+//    }
+
+
+}
