ed2527f27008c798f544e0db6236ec4fd77a9475
Kaarin1337 <kaarin.gaming@tutanota.com>
2024-11-28 11:12:08 +0100

    created a baselayout with test pages

---
+++ src/main/java/org/grp5/thetask/TestController.java
@@ -0,0 +1,19 @@
+package org.grp5.thetask;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+
+    @GetMapping("/test1")
+    public String getTest1(){
+        return "test1";
+    }
+
+    @GetMapping("/test2")
+    public String getTest2(){
+        return "test2";
+    }
+}
