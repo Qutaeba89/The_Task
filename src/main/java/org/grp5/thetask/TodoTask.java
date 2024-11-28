a4c3061e3b4f3667d2253967c58fab950f377f9a
Faraz Rostami <faraz.rostami@gmail.com>
2024-11-28 15:44:46 +0100

    adds getters for a todotask

--- src/main/java/org/grp5/thetask/TodoTask.java
+++ src/main/java/org/grp5/thetask/TodoTask.java
@@ -4,11 +4,23 @@ public class TodoTask {
     private boolean taskStatus = false; // true if the task is completed, false if the task is incomplete.
     private String title;
     private long timeInMilliSecDeadLine;
-    
+
     public TodoTask(String title, long timeInMilliSecDeadLine) {
         this.title = title;
         this.timeInMilliSecDeadLine = timeInMilliSecDeadLine;
 
+    }
+
+    public boolean isTaskStatus() {
+        return taskStatus;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public long getTimeInMilliSecDeadLine() {
+        return timeInMilliSecDeadLine;
     }
 
     public boolean isDeadLinePassed(long timeInMilliSecDeadLine){
