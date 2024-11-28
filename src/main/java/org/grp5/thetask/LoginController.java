8fc87fa99c7c63314ae04987bbcdd8dd837908ae
Niklas <Niklasgth@hotmail.com>
2024-11-28 16:06:45 +0100

    basic mappin loginCOntroller

---
+++ src/main/java/org/grp5/thetask/LoginController.java
@@ -0,0 +1,31 @@
+package org.grp5.thetask;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String showLoginPage() {
+        return "login";  // Show login page
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String loginUser(HttpSession session, Model model) {
+        // input code to verify user equals later
+        // now we use a demo user
+        session.setAttribute("username", "demoUser");
+        return "redirect:/dashboard";  // send to dashboardn
+    }
+
+    // Logout contoller if needed
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logoutUser(HttpSession session) {
+        session.invalidate();  // delete session
+        return "redirect:/login";  // return to login
+    }
+}
