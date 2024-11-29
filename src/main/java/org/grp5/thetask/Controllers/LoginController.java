07ec2547c3ba21db1a8bc847da947a9bf68f1ea8
Niklas <Niklasgth@hotmail.com>
2024-11-29 14:31:35 +0100

    Uppdaterad Loginsida
    Använder vår pretend databas och kollar mot vad som skrivs in.
    Även register länk har lagts till

--- src/main/java/org/grp5/thetask/Controllers/LoginController.java
+++ src/main/java/org/grp5/thetask/Controllers/LoginController.java
@@ -1,31 +1,42 @@
 package org.grp5.thetask.Controllers;  
 
 import jakarta.servlet.http.HttpSession;
+import org.grp5.thetask.User;
+import org.grp5.thetask.PretendDatabase; 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    // Show login page both /login and /
+    @RequestMapping(value = {"/login", "/"}, method = RequestMethod.GET)
     public String showLoginPage() {
-        return "login";  // Show login page
+        return "login"; 
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String loginUser(HttpSession session, Model model) {
-        // input code to verify user equals later
-        // now we use a demo user
-        session.setAttribute("username", "demoUser");
-        return "redirect:/dashboard";  // send to dashboardn
+    public String loginUser(HttpSession session, Model model, @RequestParam String username, @RequestParam String password) {
+        // controll un. and pw. loop through list from PD.
+        //send to dashboard if equals
+        for (User user : PretendDatabase.getUsers()) {
+            if (user.getUsername().equals(username) && user.isPasswordCorrect(password)) {
+                session.setAttribute("username", username);
+                return "redirect:/dashboard";  
+            } 
+        } 
+        model.addAttribute("error", "Fel användarnamn eller lösenord.");
+        return "login"; 
     }
 
     // Logout contoller if needed
     @RequestMapping(value = "/logout", method = RequestMethod.GET)
     public String logoutUser(HttpSession session) {
-        session.invalidate();  // delete session
-        return "redirect:/login";  // return to login
+        session.invalidate();  // deleting session
+        return "redirect:/login"; 
     }
 }
