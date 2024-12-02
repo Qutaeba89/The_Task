a478da4e1b45bbd2371dc1a68756c491c925d66f
Qutaeba89 <qutaebadandashi@gmail.com>
2024-12-02 10:24:52 +0100

    RegisterController and PretendDatabass share the same lis.

--- src/main/java/org/grp5/thetask/Controllers/RegisterController.java
+++ src/main/java/org/grp5/thetask/Controllers/RegisterController.java
@@ -1,9 +1,7 @@
 package org.grp5.thetask.Controllers;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import org.grp5.thetask.User;
+import org.grp5.thetask.PretendDatabase;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,28 +13,25 @@ import static org.grp5.thetask.Constants.Attributes.MESSAGE;
 
 @Controller
 public class RegisterController {
-    private final List<User> users = new ArrayList<>();
 
     @GetMapping("/register")
     public String registerPage() {
         return "register";
     }
 
-    @PostMapping("/register")
-    public String registerUser(@RequestParam String username, @RequestParam String password, @RequestParam String confirmePassword, Model model) {
-        for (User user : users) {
-            if (user.getUsername().equals(username)) {
-                model.addAttribute(ERROR, "Användarnamnet finns redan!");
-                return "register";
-            }
+@PostMapping("/register")
+public String registerUser(@RequestParam String username, @RequestParam String password, @RequestParam String confirmePassword, Model model) {
+    if (PretendDatabase.getUser(username)!=null) {
+        model.addAttribute(ERROR, "Användarnamnet finns redan!");
+        return "register";
         }
-        if (!password.equals(confirmePassword)) {
-            model.addAttribute(ERROR, "Lösenordet är inte samma!");
-            return "register";
-        }
-        users.add(new User(username, password));
-        model.addAttribute(MESSAGE, "Registrering Lyckades! Logga in.");
-        // return "redirect:/login";
+    if (!password.equals(confirmePassword)) {
+        model.addAttribute(ERROR, "Lösenordet är inte samma!");
         return "register";
     }
+    PretendDatabase.addUser(username,password);
+    model.addAttribute(MESSAGE, "Registrering Lyckades! Logga in.");
+    // return "redirect:/login";
+    return "register";
+}
 }
