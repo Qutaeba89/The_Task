6407846d719bbd72ef1bbd35e380f8943941caf4
Kaarin1337 <kaarin.gaming@tutanota.com>
2024-12-04 04:21:59 +0100

    work on dashboard look and feel

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Getting all checkboxes
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]'); 
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
     // Looping through the checkboxes and adding event listeners
-    checkboxes.forEach(function(checkbox) {
-        checkbox.addEventListener('change', function() {
+    checkboxes.forEach(function (checkbox) {
+        checkbox.addEventListener('change', function () {
             // Find the closest task-div element and the task label
             const taskDiv = this.closest('.task-div');
-            const taskLabel = taskDiv.querySelector('.task-left-div-top label');
-            
+            const taskLabel = taskDiv.querySelector('#task-top-left-div label');
+
             if (this.checked) {
                 // If the checkbox is checked, add the 'completed-task' class to strikethrough the text
                 taskLabel.classList.add("completed-task");
-                taskDiv.classList.add("completed-task-background"); 
+                taskDiv.classList.add("completed-task-background");
             } else {
                 // If the checkbox is unchecked, remove the 'completed-task' class to remove the strikethrough
                 taskLabel.classList.remove("completed-task");
