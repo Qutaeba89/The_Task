0de89b3ac719d9d49159f488097d32a39a27765a
Farazros <faraz.rostami@gmail.com>
2024-12-05 15:14:58 +0100

    removes unused imports and adds one test

--- src/test/java/org/grp5/thetask/TheTaskApplicationTests.java
+++ src/test/java/org/grp5/thetask/TheTaskApplicationTests.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.grp5.thetask.ToDos.TodoList;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -30,19 +31,19 @@ class TheTaskApplicationTests {
 //    }
 
 
-    // @Test
-    // public void testAddTaskIncreasesSize() {
-    //     // Adds an empty list for new tasks
-    //     TodoList newList = new TodoList();
+     @Test
+     public void testAddTaskIncreasesSize() {
+         // Create a new instance of TodoList
+         TodoList todoList = new TodoList(1, "Test List");
 
-    //     // Adds a new task to the list
-    //     int initialSize = newList.getTodoTasks().size(); // Gets the initial size of tasks list
-    //     newList.createTodoTask(null, 0); // A new task is added to the list
-    //     int newSize = newList.getTodoTasks().size(); // Get the new size of tasks list
+         // Get initial size, adds a new task to the list and gets the new size
+         int initialSize = todoList.getTasks().size();
+         todoList.createTodoTask("Test Task", System.currentTimeMillis());
+         int newSize = todoList.getTasks().size();
 
-    //     // Controls that the initial size has been incremented by 1
-    //     assertEquals(initialSize + 1, newSize);
-    // }
+    //  Controls that the initial size has been incremented by 1
+        assertEquals(initialSize + 1, newSize);
+     }
 
 
     // @Test
